6a7d6de8480e7b9baf74692935fadd302694e4f5
Uggla <uggla@free.fr>
2022-04-24 00:35:22 +0200

    feat: Refactor starfield
    - Create a module for starfield

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,21 @@
 mod screen;
+mod stars;
 use macroquad::prelude::*;
-use screen::center;
-
-const MAX_SIZE: f32 = 8.0;
-const MAX_STAR: usize = 200;
-
-struct Star {
-    pos: Vec2,
-    size: f32,
-    vel: Vec2,
-    color: Color,
-}
-
-impl Star {
-    fn new() -> Self {
-        Self {
-            pos: Vec2::ZERO,
-            size: 3.0,
-            vel: gen_vel(),
-            color: BLACK,
-        }
-    }
-
-    fn draw(&self) {
-        let center = center();
-        let pos = center + self.pos;
-        draw_circle(pos.x, pos.y, self.size, self.color)
-    }
-
-    fn update_size(&mut self) {
-        self.size = self.pos.y.abs() * MAX_SIZE / (screen_height() / 2.0);
-    }
-
-    fn update_color(&mut self) {
-        let color = self.pos.y.abs() * 1.0 / (screen_height() / 2.0);
-        self.color = Color {
-            r: color,
-            g: color,
-            b: color,
-            a: 1.0,
-        };
-    }
-
-    fn update_pos(&mut self) {
-        self.pos += self.vel;
-        self.update_size();
-        self.update_color();
-        if self.pos.x.abs() > screen_width() / 2.0 || self.pos.y.abs() > screen_height() / 2.0 {
-            self.pos = Vec2::ZERO;
-            self.vel = gen_vel();
-            self.color = BLACK;
-        }
-    }
-}
-
-fn gen_vel() -> Vec2 {
-    Vec2::new(rand::gen_range(-10.0, 10.0), rand::gen_range(-10.0, 10.0))
-}
+use stars::Starfield;
 
 #[macroquad::main("Intro")]
 async fn main() {
     rand::srand(miniquad::date::now() as u64);
 
-    let mut starfield: Vec<Star> = Vec::new();
-    let mut nstar = 0;
+    let mut starfield = Starfield::new();
 
     loop {
         clear_background(BLACK);
-        if nstar < MAX_STAR {
-            starfield.push(Star::new());
-            nstar += 1;
-        }
 
-        for star in &mut starfield {
-            star.update_pos();
-        }
+        starfield.add_star();
+        starfield.update();
+        starfield.draw();
 
-        for star in &starfield {
-            star.draw();
-        }
         next_frame().await
     }
 }
---
+++ src/stars.rs
@@ -0,0 +1,91 @@
+const MAX_SIZE: f32 = 8.0;
+const MAX_STARS: usize = 200;
+
+use crate::screen::center;
+use macroquad::prelude::*;
+
+pub struct Starfield {
+    count: usize,
+    stars: Vec<Star>,
+}
+
+impl Starfield {
+    pub fn new() -> Self {
+        Self {
+            count: 0,
+            stars: Vec::new(),
+        }
+    }
+
+    pub fn add_star(&mut self) {
+        if self.count < MAX_STARS {
+            self.stars.push(Star::new());
+            self.count += 1;
+        }
+    }
+
+    pub fn update(&mut self) {
+        for star in &mut self.stars {
+            star.update_pos();
+        }
+    }
+
+    pub fn draw(&self) {
+        for star in &self.stars {
+            star.draw();
+        }
+    }
+}
+
+struct Star {
+    pos: Vec2,
+    size: f32,
+    vel: Vec2,
+    color: Color,
+}
+
+fn gen_vel() -> Vec2 {
+    Vec2::new(rand::gen_range(-10.0, 10.0), rand::gen_range(-10.0, 10.0))
+}
+
+impl Star {
+    fn new() -> Self {
+        Self {
+            pos: Vec2::ZERO,
+            size: 3.0,
+            vel: gen_vel(),
+            color: BLACK,
+        }
+    }
+
+    fn draw(&self) {
+        let center = center();
+        let pos = center + self.pos;
+        draw_circle(pos.x, pos.y, self.size, self.color)
+    }
+
+    fn update_size(&mut self) {
+        self.size = self.pos.y.abs() * MAX_SIZE / (screen_height() / 2.0);
+    }
+
+    fn update_color(&mut self) {
+        let color = self.pos.y.abs() * 1.0 / (screen_height() / 2.0);
+        self.color = Color {
+            r: color,
+            g: color,
+            b: color,
+            a: 1.0,
+        };
+    }
+
+    fn update_pos(&mut self) {
+        self.pos += self.vel;
+        self.update_size();
+        self.update_color();
+        if self.pos.x.abs() > screen_width() / 2.0 || self.pos.y.abs() > screen_height() / 2.0 {
+            self.pos = Vec2::ZERO;
+            self.vel = gen_vel();
+            self.color = BLACK;
+        }
+    }
+}
