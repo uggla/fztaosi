d7a7a2086fa63ed9aa490abb69c31da543940a78
Uggla <uggla@free.fr>
2022-04-23 19:17:20 +0200

    feat: Center and rand

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Intro")]
 async fn main() {
+    let center_x = screen_width() / 2.0;
+    let center_y = screen_height() / 2.0;
+
+    rand::srand(miniquad::date::now() as u64);
+
     loop {
         clear_background(BLACK);
+        draw_circle(center_x, center_y, 3.0, WHITE);
 
         next_frame().await
     }
