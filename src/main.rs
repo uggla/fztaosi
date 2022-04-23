83e7be08b8c345c697af27c9c1fdfd2bd3c806e2
Uggla <uggla@free.fr>
2022-04-23 23:52:09 +0200

    feat: Update color and size

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ mod screen;
 use macroquad::prelude::*;
 use screen::center;
 
+const MAX_SIZE: f32 = 8.0;
+
 struct Star {
     pos: Vec2,
     size: f32,
     vel: Vec2,
+    color: Color,
 }
 
 impl Star {
@@ -14,17 +17,34 @@ impl Star {
             pos: Vec2::ZERO,
             size: 3.0,
             vel: gen_vel(),
+            color: BLACK,
         }
     }
 
     fn draw(&self) {
         let center = center();
         let pos = center + self.pos;
-        draw_circle(pos.x, pos.y, self.size, WHITE)
+        draw_circle(pos.x, pos.y, self.size, self.color)
+    }
+
+    fn update_size(&mut self) {
+        self.size = self.pos.y.abs() * MAX_SIZE / (screen_height() / 2.0);
+    }
+
+    fn update_color(&mut self) {
+        let color = self.pos.y.abs() * 1.0 / (screen_height() / 2.0);
+        self.color = Color {
+            r: color,
+            g: color,
+            b: color,
+            a: 1.0,
+        };
     }
 
     fn update_pos(&mut self) {
         self.pos += self.vel;
+        self.update_size();
+        self.update_color();
         if self.pos.x.abs() > screen_width() / 2.0 || self.pos.y.abs() > screen_height() / 2.0 {
             self.pos = Vec2::ZERO;
             self.vel = gen_vel();
