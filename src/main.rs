87dcc77f6cee5b9596ed1c077b634e0d0e73f8f3
Uggla <uggla@free.fr>
2022-05-15 23:05:12 +0200

    feat: Add sinus scroll, rainbow

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod text;
 
 use macroquad::prelude::*;
 use stars::Starfield;
-use std::time::Duration;
 use text::Text;
 
 #[macroquad::main("Intro")]
@@ -12,18 +11,27 @@ async fn main() {
     rand::srand(miniquad::date::now() as u64);
 
     let mut starfield = Starfield::new();
-    let mut text = Text::new("SINUS !", 70).await;
+    let mut text = Text::new("THIS IS AN OLD SCHOOL SINUS SCROLL !!!    ENJOY IT...    GREETINGS TO ROSE - TAUPIE - MENTHE...", 70).await;
+    let mut text2 = Text::new("HORIZONTAL SCROLLING...", 70).await;
 
     loop {
+        let delta = get_frame_time();
         clear_background(BLACK);
 
         starfield.add_star();
         starfield.update();
         starfield.draw();
 
-        text.draw();
+        text.draw(delta, 400.0, 0.0, true, RED, true);
+        text2.draw(
+            delta,
+            200.0,
+            screen_height() / 2.0 - 10.0,
+            false,
+            RED,
+            false,
+        );
         println!("fps: {}", get_fps());
         next_frame().await;
-        // std::thread::sleep(Duration::from_millis(10));
     }
 }
--- src/text.rs
+++ src/text.rs
@@ -26,21 +26,45 @@ impl Text {
         letters
     }
 
-    pub fn draw(&mut self) {
+    pub fn draw(
+        &mut self,
+        delta: f32,
+        speed: f32,
+        offset_y: f32,
+        sinus: bool,
+        color: Color,
+        rainbow: bool,
+    ) {
+        let rainbow_colors: Vec<Color> = vec![RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, PINK];
+        let mut rainbow_color = rainbow_colors.iter().cycle();
         let letters = self.split_text();
         let mut letter_posx = self.scroll_posx;
+        let mut letter_posy;
         for letter in letters {
+            if sinus {
+                letter_posy = screen_height() / 6.0 * (0.006 * letter_posx).sin() + offset_y;
+            } else {
+                letter_posy = offset_y;
+            }
             let dimtext = measure_text(&letter, Option::Some(self.font), self.font_size, 1.0);
-            self.draw_letter(letter, Vec2::new(letter_posx, screen_height() / 2.0 - 10.0));
+            if rainbow {
+                self.draw_letter(
+                    letter,
+                    Vec2::new(letter_posx, letter_posy),
+                    rainbow_color.next().unwrap().to_owned(),
+                );
+            } else {
+                self.draw_letter(letter, Vec2::new(letter_posx, letter_posy), color);
+            }
             letter_posx += dimtext.width;
         }
-        self.scroll_posx -= 10.0;
+        self.scroll_posx -= speed * delta;
         if letter_posx < -screen_width() / 2.0 {
             self.scroll_posx = screen_width() / 2.0;
         }
     }
 
-    fn draw_letter(&self, letter: String, pos: Vec2) {
+    fn draw_letter(&self, letter: String, pos: Vec2, color: Color) {
         let center = center();
         let pos = center + pos;
         draw_text_ex(
@@ -50,7 +74,7 @@ impl Text {
             TextParams {
                 font_size: self.font_size,
                 font: self.font,
-                color: RED,
+                color,
                 ..Default::default()
             },
         );
