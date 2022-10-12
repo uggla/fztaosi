02e0dfc0e9f7364e5f2e32a9aa8eb196bcf36c58
Uggla <uggla@free.fr>
2022-10-13 00:40:43 +0200

    feat: Use a builder for Text
    - Use a builder for Text instance.
    - Text.draw() does not need parameters anymore.

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,25 @@ async fn main() {
     rand::srand(miniquad::date::now() as u64);
 
     let mut starfield = Starfield::new();
+
     let mut text = Text::new(
         "THIS IS AN OLD SCHOOL SINUS SCROLL DONE WITH RUST AND MACROQUAD !!!    \
-                              ENJOY IT...    LAB: FROM ZERO TO AN OLD SCHOLL INTRO    \
-                              GREETINGS TO ROSE - TAUPIE - MENTHE...",
+        ENJOY IT...    LAB: FROM ZERO TO AN OLD SCHOLL INTRO    \
+        GREETINGS TO ROSE - TAUPIE - MENTHE...",
         70,
-    );
-    let mut text2 = Text::new("THANKS TO BE HERE WITH US AT FLOSSCON 2022...", 75);
+    )
+    .speed(8.0)
+    .sinus(true)
+    .color(RED)
+    .rainbow(true)
+    .build();
+
+    let mut text2 = Text::new("THANKS TO BE HERE WITH US AT FLOSSCON 2022...", 75)
+        .speed(4.0)
+        .offset_y(screen_height() / 2.0 - 10.0)
+        .color(DARKBLUE)
+        .build();
+
     let mut balls = Balls::new(0.2).await;
     let mut counter = 0.0;
     let mut music_ready = false;
@@ -38,8 +50,8 @@ async fn main() {
         balls.draw(counter);
         counter += 0.035;
 
-        text.draw(8.0, 0.0, true, RED, true);
-        text2.draw(4.0, screen_height() / 2.0 - 10.0, false, DARKBLUE, false);
+        text.draw();
+        text2.draw();
         println!("fps: {}", get_fps());
         next_frame().await;
     }
--- src/text.rs
+++ src/text.rs
@@ -6,18 +6,82 @@ pub struct Text {
     font_size: u16,
     font: Font,
     scroll_posx: f32,
+    speed: f32,
+    offset_y: f32,
+    sinus: bool,
+    color: Color,
+    rainbow: bool,
+}
+
+pub struct TextBuilder {
+    text: String,
+    font_size: u16,
+    font: Font,
+    scroll_posx: f32,
+    speed: f32,
+    offset_y: f32,
+    sinus: bool,
+    color: Color,
+    rainbow: bool,
+}
+
+impl TextBuilder {
+    pub fn speed(&mut self, speed: f32) -> &mut Self {
+        self.speed = speed;
+        self
+    }
+
+    pub fn offset_y(&mut self, offset_y: f32) -> &mut Self {
+        self.offset_y = offset_y;
+        self
+    }
+
+    pub fn sinus(&mut self, sinus: bool) -> &mut Self {
+        self.sinus = sinus;
+        self
+    }
+
+    pub fn color(&mut self, color: Color) -> &mut Self {
+        self.color = color;
+        self
+    }
+
+    pub fn rainbow(&mut self, rainbow: bool) -> &mut Self {
+        self.rainbow = rainbow;
+        self
+    }
+
+    pub fn build(&self) -> Text {
+        Text {
+            text: self.text.clone(),
+            font_size: self.font_size,
+            font: self.font,
+            scroll_posx: self.scroll_posx,
+            speed: self.speed,
+            offset_y: self.offset_y,
+            sinus: self.sinus,
+            color: self.color,
+            rainbow: self.rainbow,
+        }
+    }
 }
 
 impl Text {
-    pub fn new(msg: &str, size: u16) -> Self {
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new(msg: &str, size: u16) -> TextBuilder {
         let font = include_bytes!("../fonts/origami.ttf");
         let font = load_ttf_font_from_bytes(font).unwrap();
 
-        Self {
+        TextBuilder {
             text: msg.to_string(),
             font_size: size,
             font,
             scroll_posx: screen_width() / 2.0,
+            speed: 1.0,
+            offset_y: 0.0,
+            sinus: false,
+            color: WHITE,
+            rainbow: false,
         }
     }
 
@@ -29,31 +93,31 @@ impl Text {
         letters
     }
 
-    pub fn draw(&mut self, speed: f32, offset_y: f32, sinus: bool, color: Color, rainbow: bool) {
+    pub fn draw(&mut self) {
         let rainbow_colors: Vec<Color> = vec![RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, PINK];
         let mut rainbow_color = rainbow_colors.iter().cycle();
         let letters = self.split_text();
         let mut letter_posx = self.scroll_posx;
         let mut letter_posy;
         for letter in letters {
-            if sinus {
-                letter_posy = screen_height() / 6.0 * (0.006 * letter_posx).sin() + offset_y;
+            if self.sinus {
+                letter_posy = screen_height() / 6.0 * (0.006 * letter_posx).sin() + self.offset_y;
             } else {
-                letter_posy = offset_y;
+                letter_posy = self.offset_y;
             }
             let dimtext = measure_text(&letter, Option::Some(self.font), self.font_size, 1.0);
-            if rainbow {
+            if self.rainbow {
                 self.draw_letter(
                     letter,
                     Vec2::new(letter_posx, letter_posy),
                     rainbow_color.next().unwrap().to_owned(),
                 );
             } else {
-                self.draw_letter(letter, Vec2::new(letter_posx, letter_posy), color);
+                self.draw_letter(letter, Vec2::new(letter_posx, letter_posy), self.color);
             }
             letter_posx += dimtext.width;
         }
-        self.scroll_posx -= speed;
+        self.scroll_posx -= self.speed;
         if letter_posx < -screen_width() / 2.0 {
             self.scroll_posx = screen_width() / 2.0;
         }
